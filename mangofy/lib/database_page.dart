2eba7e0428200f22a98806d7ce53852a91fd461c
almsison <sisonalleiahlorraine@gmail.com>
2025-11-02 19:22:11 +0800

    feat: add database page

---
+++ mangofy/lib/database_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DatabasePage extends StatelessWidget {
+  const DatabasePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Dataset Page',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
