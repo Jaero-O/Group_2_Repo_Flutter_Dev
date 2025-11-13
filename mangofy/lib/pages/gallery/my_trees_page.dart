f66863870b57e95246a29bfcf010ada02d4848af
almsison <sisonalleiahlorraine@gmail.com>
2025-11-13 19:43:47 +0800

    fix: album layout

--- mangofy/lib/pages/gallery/my_trees_page.dart
+++ mangofy/lib/pages/gallery/my_trees_page.dart
@@ -4,13 +4,13 @@ import 'album_photos_page.dart';
 class MyTreesPage extends StatelessWidget {
   final bool isSelectionMode;
   final ValueChanged<String>? onAlbumSelected;
-  final List<Map<String, dynamic>> albums; 
+  final List<Map<String, dynamic>> albums;
 
   const MyTreesPage({
     super.key,
     this.isSelectionMode = false,
     this.onAlbumSelected,
-    this.albums = const [], 
+    this.albums = const [],
   });
 
   @override
@@ -18,7 +18,8 @@ class MyTreesPage extends StatelessWidget {
     final displayAlbums = albums.isEmpty
         ? [
             {
-              'title': 'My First Album',
+              'title':
+                  'My First Album with a Very Very Long Name That Should Not Overflow',
               'location': 'Backyard',
               'images': ['images/leaf.png'],
               'cover_image': 'images/leaf.png',
@@ -30,9 +31,9 @@ class MyTreesPage extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        crossAxisSpacing: 12,
-        mainAxisSpacing: 12,
-        childAspectRatio: 0.75,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 1,
+        childAspectRatio: 0.70,
       ),
       itemCount: displayAlbums.length,
       itemBuilder: (context, index) {
@@ -41,12 +42,15 @@ class MyTreesPage extends StatelessWidget {
         final location = album['location'] as String? ?? '';
         final images = album['images'] as List<String>? ?? [];
         final String coverImage;
-        if (album.containsKey('cover_image') && (album['cover_image'] as String).isNotEmpty) {
-           coverImage = album['cover_image'] as String;
-        } else if (images.isNotEmpty && (images.last.contains('/') || images.last.contains('.'))) {
+
+        if (album.containsKey('cover_image') &&
+            (album['cover_image'] as String).isNotEmpty) {
+          coverImage = album['cover_image'] as String;
+        } else if (images.isNotEmpty &&
+            (images.last.contains('/') || images.last.contains('.'))) {
           coverImage = images.last;
         } else {
-          coverImage = 'images/leaf.png'; 
+          coverImage = 'images/leaf.png';
         }
 
         return GestureDetector(
@@ -66,49 +70,63 @@ class MyTreesPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              AspectRatio(
-                aspectRatio: 1.0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withAlpha(38),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
+              Expanded(
+                flex: 7,
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withAlpha(38),
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage(coverImage),
+                        fit: BoxFit.cover,
                       ),
-                    ],
-                    image: DecorationImage(
-                      image: AssetImage(coverImage),
-                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
+
               const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.only(left: 4.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    ),
-                    if (location.isNotEmpty)
+
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
                       Text(
-                        location,
-                        style: TextStyle(
-                          color: Colors.black.withAlpha(178),
-                          fontSize: 12,
+                        title,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
                         ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: false,
                       ),
-                  ],
+                      if (location.isNotEmpty)
+                        Text(
+                          location,
+                          style: TextStyle(
+                            color: Colors.black.withAlpha(178),
+                            fontSize: 12,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                    ],
+                  ),
                 ),
               ),
             ],
