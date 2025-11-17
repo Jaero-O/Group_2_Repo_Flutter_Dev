a63a6bcf7f892b4c519cfed690cdd4d86e7b95c7
almsison <sisonalleiahlorraine@gmail.com>
2025-11-17 14:04:49 +0800

    fix: update name case of app

--- mangofy/test/widget_test.dart
+++ mangofy/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mangofy/main.dart';
+import 'package:Mangofy/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
